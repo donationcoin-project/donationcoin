005070f73251a3597a5789c08a7cce9c4f13ec29
Donationcoin <asiashibe@gmail.com>
2017-01-15 16:41:29 -0500

    Version 1.4

--- src/clientversion.h
+++ src/clientversion.h
@@ -7,7 +7,7 @@
 
 // These need to be macros, as version.cpp's and donationcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       1
-#define CLIENT_VERSION_MINOR       3
+#define CLIENT_VERSION_MINOR       4
 #define CLIENT_VERSION_REVISION    0
 #define CLIENT_VERSION_BUILD       0
 
@@ -16,7 +16,7 @@
 
 // Copyright year (2009-this)
 // Todo: update this when changing our copyright comments in the source
-#define COPYRIGHT_YEAR 2014
+#define COPYRIGHT_YEAR 2014-2017
 
 // Converts the parameter X to a string after macro replacement on X has been performed.
 // Don't merge these into one macro!
